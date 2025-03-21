84596fa2c797d45ca7e81a37350b359e26216059
carlosalberto900 <carlos.alberto.900@gmail.com>
2025-03-21 18:54:35 -0300

    Update APP_Incapacidade.py

--- APP_Incapacidade.py
+++ APP_Incapacidade.py
@@ -442,6 +442,7 @@ if 'processo_formatado' in locals():
                 doc.save(tmp.name)
                 with open(tmp.name, "rb") as f:
                     st.download_button("Download Sentença", f.read(), f"{processo}.docx")
-                    st.rerun()
+                    
+st.rerun()
                    
                     
