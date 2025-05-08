2239a9ebeb0824c75f41f1f70ec96a739fb7a70f
carlosalberto900 <carlos.alberto.900@gmail.com>
2025-05-08 15:16:16 -0300

    Update APP_Incapacidade.py

--- APP_Incapacidade.py
+++ APP_Incapacidade.py
@@ -12,9 +12,9 @@ def texto_base(doc, fundamento_questao):
         for i, paragrafo in enumerate(fundamento_base):
             parag = doc.add_paragraph(paragrafo)
             parag.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY 
-            if i in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 25, 26, 28, 31, 33, 34, 36, 37, 38, 40, 42, 43, 45, 46, 47, 49]:
+            if i in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 24, 25, 27, 30, 32, 33, 35, 36, 37, 39, 41, 42, 44, 46, 48]:
                 parag.paragraph_format.first_line_indent = Cm(2) 
-            elif i in [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 27, 29, 30, 32, 35, 39, 41, 44, 48]:
+            elif i in [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 26, 28, 29, 31, 34, 38, 40, 43, 45, 47]:
                 parag.paragraph_format.left_indent = Cm(2)
     if fundamento_questao == 2:
         for linha in fundamento_custom.split("\n"):
